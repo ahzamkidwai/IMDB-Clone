ae45908376a96dea69b2d057c5c394942ebcf844
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2024-03-01 12:44:30 +0530

    Create NavBar Component

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -13,17 +13,18 @@ function DarkModeSwitch() {
 
   return (
     <div>
-      {mounted && (currentTheme === "dark" ? (
-        <MdLightMode
-          onClick={() => setTheme("light")}
-          className="text-xl cursor-pointer hover:text-amber-500 "
-        />
-      ) : (
-        <MdDarkMode
-          onClick={() => setTheme("dark")}
-          className="text-xl cursor-pointer hover:text-amber-500 "
-        />
-      ))}
+      {mounted &&
+        (currentTheme === "dark" ? (
+          <MdLightMode
+            onClick={() => setTheme("light")}
+            className="text-xl cursor-pointer hover:text-amber-500 "
+          />
+        ) : (
+          <MdDarkMode
+            onClick={() => setTheme("dark")}
+            className="text-xl cursor-pointer hover:text-amber-500 "
+          />
+        ))}
     </div>
   );
 }
